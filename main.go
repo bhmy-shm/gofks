af1759183cf69133d5f1f434cdc94db77edc430f
孙海铭 <1136265636@qq.com>
2022-07-17 12:08:04 +0800

    first

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
+import "fmt"
 
-//func main() {
-//
-//	gofk.Ignite().
-//		Beans(db.NewGormAdapter(), cache.NewRedis()).
-//		Attach(middlewares.NewUserMid()).
-//		Mount("v1",
-//			classes.NewUserClass(),
-//			classes.NewArticleCalss()).
-//		Cron("0/3 * * * * *", func() {
-//			log.Println("每隔3秒执行一下子")
-//		}).
-//		Cron("0/2 * * * * *", func() {
-//			log.Println("每隔2秒执行一下子")
-//		}).
-//		Launch()
-//
-//}
+func main() {
+	fmt.Println("gofk")
+}
